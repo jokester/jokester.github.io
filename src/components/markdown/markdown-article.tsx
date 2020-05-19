e42391df70d4047afdfd7935b784255268de8c5f
Wang Guan <momocraft@gmail.com>
2020-05-19 19:05:05 +0900

    styles

--- src/components/markdown/markdown-article.tsx
+++ src/components/markdown/markdown-article.tsx
@@ -1,10 +1,15 @@
 import ReactMarkdown from 'react-markdown';
 import React from 'react';
 import { Prism } from 'react-syntax-highlighter';
+import codeStyle from 'react-syntax-highlighter/dist/cjs/styles/prism/atom-dark';
 
 const CustomRenderers = {
   code(prop: { language: string; value: string }) {
-    return <Prism language={prop.language}>{prop.value}</Prism>;
+    return (
+      <Prism language={prop.language} style={codeStyle}>
+        {prop.value}
+      </Prism>
+    );
   },
 } as const;
 
