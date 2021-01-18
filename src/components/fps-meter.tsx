0474a41530371a12173126648da93f831161dbca
Wang Guan <momocraft@gmail.com>
2021-01-19 00:59:08 +0900

    simplify

--- src/components/fps-meter.tsx
+++ src/components/fps-meter.tsx
@@ -1,6 +1,6 @@
 import { FC, useState } from 'react';
 import { animationFrameScheduler, interval, Observable, range } from 'rxjs';
-import { map, scan, throttleTime } from 'rxjs/operators';
+import { map, scan, tap, throttleTime } from 'rxjs/operators';
 import useConstant from 'use-constant';
 import { useObservable } from 'react-use';
 
@@ -8,27 +8,29 @@ const everyFrame = range(0, Number.MAX_SAFE_INTEGER, animationFrameScheduler);
 
 function fpsObservable(numFrame: number): Observable<number> {
   return everyFrame.pipe(
-    map((frameNo) => Date.now()),
+    map((frameNo) => Date.now() / 1e3),
     scan((acc, l) => [...acc, l].slice(-numFrame), [] as number[]),
-    map((frames) => (frames.length >= numFrame ? (numFrame * 1e3) / (frames[frames.length - 1] - frames[0]) : -1)),
+    map((frames) => (frames.length >= numFrame ? numFrame / (frames[frames.length - 1] - frames[0]) : -1)),
     throttleTime(0.5e3),
   );
 }
 
 export const FpsMeter: FC = () => {
-  const $fps5 = useConstant(() => fpsObservable(5)),
-    $fps60 = useConstant(() => fpsObservable(60)),
-    $fps300 = useConstant(() => fpsObservable(300));
-
-  const fps5 = useObservable($fps5, -1),
-    fps60 = useObservable($fps60, -1),
-    fps300 = useObservable($fps300, -1);
-
   return (
     <div className="p-4 border-black border inline-block w-64">
-      <p>fps (5frames): {fps5.toFixed(3)}</p>
-      <p>fps (60frames): {fps60.toFixed(3)}</p>
-      <p>fps (300frames): {fps300.toFixed(3)}</p>
+      <FpsMeterRow samples={15} />
+      <FpsMeterRow samples={60} />
     </div>
+  );
+};
+
+const FpsMeterRow: FC<{ samples: number }> = ({ samples }) => {
+  const fps$ = useConstant(() => fpsObservable(samples));
+  const fps = useObservable(fps$, 0);
+
+  return (
+    <p>
+      fps ({samples}frames): {fps.toFixed(3)}
+    </p>
   );
 };
--- src/components/poc-css-transform/poc-css-transform.tsx
+++ src/components/poc-css-transform/poc-css-transform.tsx
@@ -15,19 +15,16 @@ export const PocCssTransform: FC = () => {
   const [items, setItems] = useState<FlyingText[]>([]);
 
   useEffect(() => {
-    const timer = interval(0.1e3, animationFrameScheduler);
+    const timer = interval(0.5e3);
 
-    const itemSource = timer.pipe(map((_) => ({ nonce: randomHex(6), text: `click #${_}` })));
-    const subscribe = itemSource.subscribe((newItem) => setItems((prev) => [...prev, newItem].slice(-30)));
+    const itemSource = timer.pipe(map((_) => ({ nonce: randomHex(6), text: `item #${_}` })));
+    const subscribe = itemSource.subscribe((newItem) => setItems((prev) => [...prev, newItem].slice(-50)));
     return () => subscribe.unsubscribe();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <div
-      onClick={() => clicked.next(1 + clicked.value)}
-      className="h-64 relative flex justify-center items-center bg-gray-900"
-    >
+    <div onClick={() => clicked.next(1 + clicked.value)} className="h-64 relative flex justify-center items-center ">
       {items.map((item) => (
         <FlyingTextLazy item={item} key={item.nonce} />
       ))}
@@ -45,7 +42,7 @@ const FlyingTextRaw: FC<{ item: FlyingText }> = (props) => {
     p.style.transform = `translateY(${randomness.y()}px) translateX(${randomness.x()}px)`;
 
     requestAnimationFrame(() => {
-      p.style.transition = 'ease-in 5s all';
+      p.style.transition = 'ease-out 5s all';
       p.style.filter = `blur(5px) opacity(0)`;
       p.style.transform = `translateY(${randomness.y()}px) translateX(${randomness.x()}px) translateZ(${randomness.z()}px)`;
     });
